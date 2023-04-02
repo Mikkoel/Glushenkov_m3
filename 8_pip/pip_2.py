180f328fe591018d9be56a74e5eeea1c2eacf01a
Mikkoel <kolromll85@gmail.com>
2023-04-02 17:04:15 +0300

    create

--- 8_pip/pip_2.py
+++ 8_pip/pip_2.py
@@ -1,5 +1,14 @@
 def pin(pins: list[int]) -> int:
     sorted(pins)
     pin_len = []
+    intic = 0
     for i in range(1, len(pins) + 1):
         pin_len.append(pins[i] - pins[i - 1])
+        if intic > 1:
+            min(pin_len[i], pin_len[i-1])
+        intic += 1
+    sum_pin_len = sum(pin_len)
+    return sum_pin_len
+
+
+print(pin([3, 8, 10, 11, 15, 17]))
--- 9_pip/pip_1.1.py
+++ 9_pip/pip_1.1.py
@@ -1,10 +1,26 @@
+
 def pin(pins: list[int]) -> int:
     pins.sort()
-    pins_len = []
-    for i in range(len(pins) + 1):
+    pins_len = [pins[0] + pins[1]]
+    nets = round(len(pins) / 2)
+    pin_nets = 1
+    pin_net = [1, 0]
+    i = 1
+    end = nets - 1
+    while nets != pin_nets and pin_net[-1] != 1:
+        much = (pins[i + 1] - pins[i])
+        mini = (pins[i] - pins[i - 1])
+        if pin_net[i] != 0 and much > mini:
+            pins_len.append(pins[i - 1] - pins[i])
+            pin_nets += 1
+            pin_net[i] = 1
+        elif pin_net[i] != 0 and much <= mini:
+            pins_len.append((pins[i] - pins[i - 1]) + pins_len[i])
+            pin_nets += 1
+            pin_net[i] = 1
+        i += 1
+        pin_net.append(0)
+    return pins_len[-1]
 
-        pins_len.append(pins[i] + pins[i + 1])
 
-
-
-print(pin([30, 5, 12, 21, 20, 14]))
+print(pin([20, 5, 12, 21, 20, 14]))
